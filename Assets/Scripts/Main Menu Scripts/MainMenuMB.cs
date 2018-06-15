c352fc64cf2334060d2016d241aedf85686ad05b
Richard Lizana <rwlizana89@hotmail.com>
2018-06-15 11:48:30 -0400

    Adding Title Scene Menus

--- Assets/Scripts/Main Menu Scripts/MainMenuMB.cs
+++ Assets/Scripts/Main Menu Scripts/MainMenuMB.cs
@@ -5,19 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuMB : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject titleSplash = null;
+    [SerializeField]
+    private GameObject mainMenu = null;
+    [SerializeField]
+    private GameObject optionsMenu;
 
     // Use this for initialization
     void Start()
     {
-
+        titleSplash.SetActive(true);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey || Input.touchCount > 0)
+        if (titleSplash.activeSelf)
         {
-            SceneManager.LoadScene("GameScene");
+            WaitForTouch();
         }
+    }
+
+    private void WaitForTouch()
+    {
+        if (Input.anyKey)
+        {
+            titleSplash.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+
+    public void StartTwoPlayerGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Main Menu Scripts/OptionsMenuMB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuMB : MonoBehaviour
+{
+
+    private bool musicActive = true;
+    private bool soundFXActive = true;
+
+    public void ToggleGameMusic()
+    {
+        musicActive = !musicActive;
+    }
+
+    public void ToggleSoundFX()
+    {
+        soundFXActive = !soundFXActive;
+    }
+
+    public void CloseMenu()
+    {
+        this.gameObject.SetActive(false);
+    }
+}

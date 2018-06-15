cf037e09d0c97bc19e1602171c13ec4272605136
Richard Lizana <rwlizana89@hotmail.com>
2018-06-15 11:47:39 -0400

    Adding Flying Cookie Effect and Updating Particles

---
+++ Assets/Scripts/Effects/FlyingCookieController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCookieController : MonoBehaviour
+{
+    [SerializeField]
+    private float cookieSpeed = 2f;
+    [SerializeField]
+    private Transform cameraBounds;
+
+    private Vector3 startPosition = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * cookieSpeed * Time.deltaTime;
+        if (transform.position.x > cameraBounds.position.x)
+        {
+            GetComponentInChildren<ParticleSystem>().Stop();
+            transform.position = startPosition;
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+    }
+}

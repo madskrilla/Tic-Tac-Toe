b451a6ad0a8590282a6d67746c35fcaee078fbca
Richard Lizana <rwlizana89@hotmail.com>
2018-06-19 14:19:23 -0400

    Adding Sound Manager to Title Scene

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Linq;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private SoundContainer sounds;
+
+    private static AudioManager inst;
+    public static AudioManager GetInstance
+    {
+        get { return inst; }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (inst != null)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        Sound s;
+        var list = sounds.SoundList;
+        for (int i = 0; i < list.Length; i++)
+        {
+            s = list[i];
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.soundClip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+
+        inst = this;
+    }
+
+    public void PlaySound(string Name)
+    {
+        Sound s = sounds.SoundList.First(x => x.name == Name);
+        if (s != null)
+        {
+            s.source.Play();
+        }
+    }
+
+    public void StopSound(string Name)
+    {
+        Sound s = sounds.SoundList.First(x => x.name == Name);
+        if (s != null)
+        {
+            s.source.Stop();
+        }
+    }
+
+    public void StopAllSounds()
+    {
+        var list = sounds.SoundList;
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i].source.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/SoundContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+class Sound
+{
+    public string name;
+    public AudioClip soundClip;
+    [Range(0, 1f)]
+    public float volume;
+    public bool loop;
+    public AudioSource source;
+}
+
+[CreateAssetMenu(fileName = "SoundList", menuName = "Settings/SoundList")]
+class SoundContainer : ScriptableObject
+{
+    public Sound[] SoundList;
+}
+
